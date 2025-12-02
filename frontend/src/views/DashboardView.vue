12aa236077d7df0022a07a3d5812e64843619fc3
jyh21377063 <21377063@buaa.edu.cn>
2025-12-02 23:54:09 +0800

    geo color

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -805,9 +805,9 @@ const renderMap = () => {
       zoom: 1.2,
       label: {emphasis: {show: false}},
       roam: true,
-      // itemStyle: {normal: {areaColor: '#1067b3', borderColor: '#ece8e8'}, emphasis: {areaColor: '#2a333d'}}
+      itemStyle: {normal: {areaColor: 'rgb(37,62,166)', borderColor: '#ece8e8'}, emphasis: {areaColor: '#2a333d'}}
       //地图颜色
-      itemStyle: {normal: {areaColor: '#2a333d', borderColor: '#ece8e8'}, emphasis: {areaColor: '#1067b3'}}
+      // itemStyle: {normal: {areaColor: '#2a333d', borderColor: '#ece8e8'}, emphasis: {areaColor: '#1067b3'}}
     },
     series: [
       {
@@ -834,7 +834,7 @@ const renderMap = () => {
         coordinateSystem: 'geo',
         zlevel: 1,
         effect: {show: true, period: 4, trailLength: 0.02, symbol: 'arrow', symbolSize: 5},
-        lineStyle: {normal: {color: '#ffeea9', width: 1, opacity: 0.6, curveness: 0.2}},
+        lineStyle: {normal: {color: 'rgba(255,238,169,0.73)', width: 1, opacity: 0.6, curveness: 0.2}},
         data: convertData(filteredDatas)
       }]
   };
